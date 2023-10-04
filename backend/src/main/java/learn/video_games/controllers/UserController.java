47ff51e3d1b64f66a9809085f5c546881a5e0256
karkrish2009 <karkrish2009@gmail.com>
2023-10-04 11:56:28 -0700

    creating backend framework

---
+++ backend/src/main/java/learn/video_games/controllers/UserController.java
@@ -0,0 +1,25 @@
+package learn.video_games.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody User user) {
+        Result<Void> result = service.addUser(user);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+}
