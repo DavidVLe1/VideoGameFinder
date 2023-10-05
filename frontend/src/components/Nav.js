89d1c7a4ee67ac6ea01daf70c47ce0c2730cbfa2
LoganBlakeCodes <122075213+LoganBlakeCodes@users.noreply.github.com>
2023-10-05 14:31:42 -0400

    Updated the Nav bar to have a fun retro game-y font.

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -5,7 +5,7 @@ export default function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <NavLink className="navbar-brand" to="/">
+        <NavLink className="navbar-brand vt323-font" to="/">
           VideoGameFinder
         </NavLink>
         <button
@@ -22,27 +22,27 @@ export default function Nav() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" exact to="/">
+              <NavLink className="nav-link vt323-font" exact to="/">
                 Home
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/library">
+              <NavLink className="nav-link vt323-font" to="/library">
                 Library
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/preferences">
+              <NavLink className="nav-link vt323-font" to="/preferences">
                 Game Form
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/signup">
+              <NavLink className="nav-link vt323-font" to="/signup">
                 Sign Up
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/signin">
+              <NavLink className="nav-link vt323-font" to="/signin">
                 Sign in
               </NavLink>
             </li>
