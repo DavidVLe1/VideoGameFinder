0fd17e509ba716ea527473e913ee3c1b9cb6b471
LoganBlakeCodes <122075213+LoganBlakeCodes@users.noreply.github.com>
2023-10-05 11:55:26 -0400

    Added a sign in and landing page

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,1 +1,11 @@
-<h1>SIGN IN PAGE.</h1>
+import React from "react"
+
+const SignIn= () => {
+    return (
+      <div>
+        <h1>SIGN IN PAGE.</h1>
+      </div>
+    );
+  };
+
+  export default SignIn;
