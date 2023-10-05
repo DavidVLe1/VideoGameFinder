4b743191548af9c280a9694005695592fd70d85e
LoganBlakeCodes <122075213+LoganBlakeCodes@users.noreply.github.com>
2023-10-05 12:35:38 -0400

    Put bootstrap into the html file, and added to the navbar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,22 +1,39 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <>
-      <nav className="navbar navbar-expand">
-        <div className="d-flex">
-          <NavLink className="navbar-brand" to="/">
-            VideoGameFinder
-          </NavLink>
-          <ul className="navbar-nav">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">
+          VideoGameFinder
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/">
+              <NavLink className="nav-link" exact to="/">
                 Home
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/">
+              <NavLink className="nav-link" to="/library">
                 Library
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/preferences">
+                Game Form
               </NavLink>
             </li>
             <li className="nav-item">
@@ -31,7 +48,7 @@ export default function Nav() {
             </li>
           </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
