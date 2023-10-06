dbc6da0dbdad9332a1fedf1f888204bd7890df0a
David Le <le.davidv18@gmail.com>
2023-10-06 10:33:01 -0500

    stubbed out what sign up component should look like.

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,11 +1,89 @@
-import React from "react"
+import { useState } from "react";
 
-const SignUp= () => {
-    return (
-      <div>
-        <h1>SIGN UP PAGE.</h1>
-      </div>
-    );
+export default function SignUp() {
+  const [signUpFormData, setSignUpFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setSignUpFormData({ ...signUpFormData, [name]: value });
   };
 
-  export default SignUp;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form Data:", signUpFormData);
+    //attempt to sign up user---attempt to create user on the backend.
+    //if failure let user know that they were unable to sign up. and maybe list reasons why.
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="firstName" className="form-label">
+            First Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="firstName"
+            name="firstName"
+            value={signUpFormData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="lastName" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            name="lastName"
+            value={signUpFormData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={signUpFormData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={signUpFormData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+}
