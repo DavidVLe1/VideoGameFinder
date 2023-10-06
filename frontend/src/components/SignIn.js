d2ac8a3edeccfd45abbaaa57f3ce0166c8d3ef1a
David Le <le.davidv18@gmail.com>
2023-10-06 10:58:01 -0500

    stubbed out what sign in component should be.

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,11 +1,66 @@
 import React from "react"
+import { useState } from "react";
 
-const SignIn= () => {
-    return (
-      <div>
-        <h1>SIGN IN PAGE.</h1>
-      </div>
-    );
+export default function SignIn() {
+  const [signInFormData, setSignInFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);//will set to true upon sign in being successful
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setSignInFormData({ ...signInFormData, [name]: value });
   };
 
-  export default SignIn;
+  const handleSignIn = async (event) => {
+    event.preventDefault();
+    console.log(signInFormData);
+    //attempt to sign in, if successful, set isAuthentication to true.
+    //if failure, give message of sign in failed.
+
+  };
+
+  return (
+    <div>
+      {isAuthenticated ? (
+        <p>You are logged in. [Log Out]</p>
+      ) : (
+        <form onSubmit={handleSignIn}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={signInFormData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={signInFormData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Sign In
+          </button>
+        </form>
+      )}
+    </div>
+  );
+}
