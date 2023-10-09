a3152582fd2ca5733f2fc68d5bcf0ab84d2776e8
LoganBlakeCodes <122075213+LoganBlakeCodes@users.noreply.github.com>
2023-10-09 02:46:22 -0400

    Deleted library.css and added all the content to app.css

--- frontend/src/components/Library.js
+++ frontend/src/components/Library.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import '../Library.css';
+import { Link } from 'react-router-dom'; 
+import '../App.css';
 
 function Library({ gamesData }) {
   return (
     <div className="library-container">
-      <h1 style={{ backgroundColor: "black", textAlign: "center", fontFamily: "'Press Start 2P', sans-serif"}}>Your Library</h1>
+      <h1 style={{ backgroundColor: "black", textAlign: "center", fontFamily: "'Press Start 2P', sans-serif" }}>Your Library</h1>
       <div className="game-cards-container">
         {gamesData.map((game, index) => (
-          <div className="game-card" key={index}>
-            <div className="game-card-image">
-              <img src={game.background_image} alt={game.name} />
-              <div className="game-name-overlay">
-                <p>{game.name}</p>
+          <Link to={`/game/${game.id}`} key={game.id}>
+            <div className="game-card">
+              <div className="game-card-image">
+                <img src={game.background_image} alt={game.name} />
+                <div className="game-name-overlay">
+                  <p>{game.name}</p>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
