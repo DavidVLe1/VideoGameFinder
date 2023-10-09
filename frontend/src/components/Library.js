2c091f91401e7ea6c372db680e5ba898c58e69d1
LoganBlakeCodes <122075213+LoganBlakeCodes@users.noreply.github.com>
2023-10-09 00:59:23 -0400

    Did the library, Has cards. Stretch goal to make each one a link to their own page with their own metadata displayed

--- frontend/src/components/Library.js
+++ frontend/src/components/Library.js
@@ -1,10 +1,24 @@
-export default function Library({ gamesData }) {
-    console.log("Games Data:", gamesData);
-  
-    return (
-      <div>
-        <h1>Library Component</h1>
-        <pre>{JSON.stringify(gamesData, null, 2)}</pre>
+import React from 'react';
+import '../Library.css'; 
+
+function Library({ gamesData }) {
+  return (
+    <div className="library-container">
+      <h1 style={{ textAlign: "center", fontFamily: "'Press Start 2P', sans-serif"}}>Library Component</h1>
+      <div className="game-cards-container">
+        {gamesData.map((game, index) => (
+          <div className="game-card" key={index}>
+            <div className="game-card-image">
+              <img src={game.background_image} alt={game.name} />
+              <div className="game-name-overlay">
+                <p>{game.name}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+export default Library;
